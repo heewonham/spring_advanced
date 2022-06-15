ee0fa027178ee1bb0efd90f10dca87abf43b9d93
heather-ham <heather.ham@kakaoenterprise.com>
2022-06-15 22:10:59 +0900

    스프링 핵심원리 고급 - 2강 threadLocal

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,66 @@
+package hello.advanced.trace;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    @DisplayName("동시성 문제가 발생하지 않음")
+    void field(){
+        log.info("main Strat");
+        Runnable userA = () -> {
+          fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);
+        threadB.start();
+        sleep(2000);
+        log.info("main exit");
+    }
+
+    @Test
+    @DisplayName("동시성이 발생하는 경우")
+    void field2(){
+        log.info("main Strat");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(200);
+        threadB.start();
+        sleep(200);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
